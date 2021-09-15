26c200a84143fad7cc30978573003570c5a3b622
mikeajohns <41806211+mikeajohns@users.noreply.github.com>
2021-09-14 20:25:10 -0500

    stable
    all 3 APIs are working and so is the faker for them.
    
    loading const data into the current weather table

--- GCP/HW6/main.py
+++ GCP/HW6/main.py
@@ -26,12 +26,23 @@ def send_webfiles(path):
     return send_from_directory('web', path)
 
 
+
 # https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=sRWfsYY1xHVnFN9f6ILSV5fVVlMQgn1O
 # https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY}
 @app.route('/apis/<api_name>', methods=['GET', 'POST'])
-def call_api(api_name):
-    req_data = request.get_json()
+def call_api(api_name, short_circuit_req_data=None):
+    if not short_circuit_req_data:
+        req_data = request.get_json()
+    else:
+        req_data = short_circuit_req_data
 
+    debug_save_usage = False
+    debug_save_usage = True
+    if debug_save_usage:
+        return get_fake_json(api_name)
+
+
+    # NOTE: the keys/tokens below should not be shared with anyone
     api_infos = {
         "tomorrow": {
             "url": "https://api.tomorrow.io/v4",
@@ -40,6 +51,10 @@ def call_api(api_name):
         "geocode": {
             "url": "https://maps.googleapis.com/maps/api/geocode",
             "params": {"key": "AIzaSyAoLc9k_wqEQNd9R-a9Skhqtl92gTHbfTc"}
+        },
+        "ipinfo": {
+            "url": "https://ipinfo.io/" + request.remote_addr,
+            "params": {"token": "6292e80abfdebd"}
         }
     }
 
@@ -66,8 +81,187 @@ def call_api(api_name):
     import requests
     response = requests.get(url)
 
+    #print(response.json())
+
+    #if api_name == "geocode":
+     #   return call_api("tomorrow", {ln})
+
+    #elif api_name == "ipinfo":
+     #   pass
+
+
     return response.json()
+
+
+def get_fake_json(api_name):
+    apis = {
+        "tomorrow": {
+            'data': {'timelines': [
+                {'timestep': '1h', 'startTime': '2021-09-15T00:00:00Z', 'endTime': '2021-09-19T12:00:00Z',
+                 'intervals': [{'startTime': '2021-09-15T00:00:00Z', 'values': {'temperature': 21.38}},
+                               {'startTime': '2021-09-15T01:00:00Z', 'values': {'temperature': 21.15}},
+                               {'startTime': '2021-09-15T02:00:00Z', 'values': {'temperature': 21.86}},
+                               {'startTime': '2021-09-15T03:00:00Z', 'values': {'temperature': 22.53}},
+                               {'startTime': '2021-09-15T04:00:00Z', 'values': {'temperature': 22.71}},
+                               {'startTime': '2021-09-15T05:00:00Z', 'values': {'temperature': 22.74}},
+                               {'startTime': '2021-09-15T06:00:00Z', 'values': {'temperature': 21.98}},
+                               {'startTime': '2021-09-15T07:00:00Z', 'values': {'temperature': 21.69}},
+                               {'startTime': '2021-09-15T08:00:00Z', 'values': {'temperature': 21.21}},
+                               {'startTime': '2021-09-15T09:00:00Z', 'values': {'temperature': 20.75}},
+                               {'startTime': '2021-09-15T10:00:00Z', 'values': {'temperature': 20.57}},
+                               {'startTime': '2021-09-15T11:00:00Z', 'values': {'temperature': 20.47}},
+                               {'startTime': '2021-09-15T12:00:00Z', 'values': {'temperature': 22.02}},
+                               {'startTime': '2021-09-15T13:00:00Z', 'values': {'temperature': 23.73}},
+                               {'startTime': '2021-09-15T14:00:00Z', 'values': {'temperature': 24.87}},
+                               {'startTime': '2021-09-15T15:00:00Z', 'values': {'temperature': 26.38}},
+                               {'startTime': '2021-09-15T16:00:00Z', 'values': {'temperature': 27.89}},
+                               {'startTime': '2021-09-15T17:00:00Z', 'values': {'temperature': 29.79}},
+                               {'startTime': '2021-09-15T18:00:00Z', 'values': {'temperature': 30.91}},
+                               {'startTime': '2021-09-15T19:00:00Z', 'values': {'temperature': 30.24}},
+                               {'startTime': '2021-09-15T20:00:00Z', 'values': {'temperature': 29.75}},
+                               {'startTime': '2021-09-15T21:00:00Z', 'values': {'temperature': 28.42}},
+                               {'startTime': '2021-09-15T22:00:00Z', 'values': {'temperature': 26.77}},
+                               {'startTime': '2021-09-15T23:00:00Z', 'values': {'temperature': 25.73}},
+                               {'startTime': '2021-09-16T00:00:00Z', 'values': {'temperature': 25.58}},
+                               {'startTime': '2021-09-16T01:00:00Z', 'values': {'temperature': 24.33}},
+                               {'startTime': '2021-09-16T02:00:00Z', 'values': {'temperature': 22.83}},
+                               {'startTime': '2021-09-16T03:00:00Z', 'values': {'temperature': 22.61}},
+                               {'startTime': '2021-09-16T04:00:00Z', 'values': {'temperature': 22.13}},
+                               {'startTime': '2021-09-16T05:00:00Z', 'values': {'temperature': 21.71}},
+                               {'startTime': '2021-09-16T06:00:00Z', 'values': {'temperature': 21.24}},
+                               {'startTime': '2021-09-16T07:00:00Z', 'values': {'temperature': 20.99}},
+                               {'startTime': '2021-09-16T08:00:00Z', 'values': {'temperature': 20.61}},
+                               {'startTime': '2021-09-16T09:00:00Z', 'values': {'temperature': 20.5}},
+                               {'startTime': '2021-09-16T10:00:00Z', 'values': {'temperature': 20.85}},
+                               {'startTime': '2021-09-16T11:00:00Z', 'values': {'temperature': 20.84}},
+                               {'startTime': '2021-09-16T12:00:00Z', 'values': {'temperature': 21.49}},
+                               {'startTime': '2021-09-16T13:00:00Z', 'values': {'temperature': 22.36}},
+                               {'startTime': '2021-09-16T14:00:00Z', 'values': {'temperature': 24.15}},
+                               {'startTime': '2021-09-16T15:00:00Z', 'values': {'temperature': 24.96}},
+                               {'startTime': '2021-09-16T16:00:00Z', 'values': {'temperature': 25.15}},
+                               {'startTime': '2021-09-16T17:00:00Z', 'values': {'temperature': 25.01}},
+                               {'startTime': '2021-09-16T18:00:00Z', 'values': {'temperature': 25.21}},
+                               {'startTime': '2021-09-16T19:00:00Z', 'values': {'temperature': 23.76}},
+                               {'startTime': '2021-09-16T20:00:00Z', 'values': {'temperature': 24.57}},
+                               {'startTime': '2021-09-16T21:00:00Z', 'values': {'temperature': 23.69}},
+                               {'startTime': '2021-09-16T22:00:00Z', 'values': {'temperature': 22.37}},
+                               {'startTime': '2021-09-16T23:00:00Z', 'values': {'temperature': 21.78}},
+                               {'startTime': '2021-09-17T00:00:00Z', 'values': {'temperature': 21.91}},
+                               {'startTime': '2021-09-17T01:00:00Z', 'values': {'temperature': 22.02}},
+                               {'startTime': '2021-09-17T02:00:00Z', 'values': {'temperature': 22.2}},
+                               {'startTime': '2021-09-17T03:00:00Z', 'values': {'temperature': 22.08}},
+                               {'startTime': '2021-09-17T04:00:00Z', 'values': {'temperature': 22}},
+                               {'startTime': '2021-09-17T05:00:00Z', 'values': {'temperature': 22.01}},
+                               {'startTime': '2021-09-17T06:00:00Z', 'values': {'temperature': 21.91}},
+                               {'startTime': '2021-09-17T07:00:00Z', 'values': {'temperature': 21.84}},
+                               {'startTime': '2021-09-17T08:00:00Z', 'values': {'temperature': 21.88}},
+                               {'startTime': '2021-09-17T09:00:00Z', 'values': {'temperature': 21.95}},
+                               {'startTime': '2021-09-17T10:00:00Z', 'values': {'temperature': 21.92}},
+                               {'startTime': '2021-09-17T11:00:00Z', 'values': {'temperature': 22.01}},
+                               {'startTime': '2021-09-17T12:00:00Z', 'values': {'temperature': 22.42}},
+                               {'startTime': '2021-09-17T13:00:00Z', 'values': {'temperature': 23.2}},
+                               {'startTime': '2021-09-17T14:00:00Z', 'values': {'temperature': 23.82}},
+                               {'startTime': '2021-09-17T15:00:00Z', 'values': {'temperature': 24.42}},
+                               {'startTime': '2021-09-17T16:00:00Z', 'values': {'temperature': 24.02}},
+                               {'startTime': '2021-09-17T17:00:00Z', 'values': {'temperature': 23.43}},
+                               {'startTime': '2021-09-17T18:00:00Z', 'values': {'temperature': 23.61}},
+                               {'startTime': '2021-09-17T19:00:00Z', 'values': {'temperature': 23.49}},
+                               {'startTime': '2021-09-17T20:00:00Z', 'values': {'temperature': 23.46}},
+                               {'startTime': '2021-09-17T21:00:00Z', 'values': {'temperature': 23.5}},
+                               {'startTime': '2021-09-17T22:00:00Z', 'values': {'temperature': 23.12}},
+                               {'startTime': '2021-09-17T23:00:00Z', 'values': {'temperature': 22.83}},
+                               {'startTime': '2021-09-18T00:00:00Z', 'values': {'temperature': 22.46}},
+                               {'startTime': '2021-09-18T01:00:00Z', 'values': {'temperature': 22.04}},
+                               {'startTime': '2021-09-18T02:00:00Z', 'values': {'temperature': 21.63}},
+                               {'startTime': '2021-09-18T03:00:00Z', 'values': {'temperature': 21.37}},
+                               {'startTime': '2021-09-18T04:00:00Z', 'values': {'temperature': 21.03}},
+                               {'startTime': '2021-09-18T05:00:00Z', 'values': {'temperature': 20.77}},
+                               {'startTime': '2021-09-18T06:00:00Z', 'values': {'temperature': 20.28}},
+                               {'startTime': '2021-09-18T07:00:00Z', 'values': {'temperature': 19.75}},
+                               {'startTime': '2021-09-18T08:00:00Z', 'values': {'temperature': 19.48}},
+                               {'startTime': '2021-09-18T09:00:00Z', 'values': {'temperature': 19.42}},
+                               {'startTime': '2021-09-18T10:00:00Z', 'values': {'temperature': 19.29}},
+                               {'startTime': '2021-09-18T11:00:00Z', 'values': {'temperature': 19.29}},
+                               {'startTime': '2021-09-18T12:00:00Z', 'values': {'temperature': 20.29}},
+                               {'startTime': '2021-09-18T13:00:00Z', 'values': {'temperature': 22.07}},
+                               {'startTime': '2021-09-18T14:00:00Z', 'values': {'temperature': 23.83}},
+                               {'startTime': '2021-09-18T15:00:00Z', 'values': {'temperature': 25.45}},
+                               {'startTime': '2021-09-18T16:00:00Z', 'values': {'temperature': 27.01}},
+                               {'startTime': '2021-09-18T17:00:00Z', 'values': {'temperature': 28.37}},
+                               {'startTime': '2021-09-18T18:00:00Z', 'values': {'temperature': 29.19}},
+                               {'startTime': '2021-09-18T19:00:00Z', 'values': {'temperature': 28.82}},
+                               {'startTime': '2021-09-18T20:00:00Z', 'values': {'temperature': 27.92}},
+                               {'startTime': '2021-09-18T21:00:00Z', 'values': {'temperature': 27.98}},
+                               {'startTime': '2021-09-18T22:00:00Z', 'values': {'temperature': 27.97}},
+                               {'startTime': '2021-09-18T23:00:00Z', 'values': {'temperature': 26.13}},
+                               {'startTime': '2021-09-19T00:00:00Z', 'values': {'temperature': 25.67}},
+                               {'startTime': '2021-09-19T01:00:00Z', 'values': {'temperature': 25.39}},
+                               {'startTime': '2021-09-19T02:00:00Z', 'values': {'temperature': 24.74}},
+                               {'startTime': '2021-09-19T03:00:00Z', 'values': {'temperature': 24.31}},
+                               {'startTime': '2021-09-19T04:00:00Z', 'values': {'temperature': 23.8}},
+                               {'startTime': '2021-09-19T05:00:00Z', 'values': {'temperature': 23.34}},
+                               {'startTime': '2021-09-19T06:00:00Z', 'values': {'temperature': 22.91}},
+                               {'startTime': '2021-09-19T07:00:00Z', 'values': {'temperature': 22.57}},
+                               {'startTime': '2021-09-19T08:00:00Z', 'values': {'temperature': 22.28}},
+                               {'startTime': '2021-09-19T09:00:00Z', 'values': {'temperature': 21.88}},
+                               {'startTime': '2021-09-19T10:00:00Z', 'values': {'temperature': 21.49}},
+                               {'startTime': '2021-09-19T11:00:00Z', 'values': {'temperature': 21.1}},
+                               {'startTime': '2021-09-19T12:00:00Z', 'values': {'temperature': 21.77}}]}]}
+
+        },
+        "geocode": {
+            'results': [{
+                'address_components': [
+                        {'long_name': 'Google Building 40', 'short_name': 'Google Building 40', 'types': ['premise']},
+                        {'long_name': '1600', 'short_name': '1600', 'types': ['street_number']},
+                        {'long_name': 'Amphitheatre Parkway', 'short_name': 'Amphitheatre Pkwy', 'types': ['route']},
+                        {'long_name': 'Mountain View', 'short_name': 'Mountain View', 'types': ['locality', 'political']},
+                        {'long_name': 'Santa Clara County', 'short_name': 'Santa Clara County', 'types': ['administrative_area_level_2', 'political']},
+                        {'long_name': 'California', 'short_name': 'CA', 'types': ['administrative_area_level_1', 'political']},
+                        {'long_name': 'United States', 'short_name': 'US', 'types': ['country', 'political']},
+                        {'long_name': '94043', 'short_name': '94043', 'types': ['postal_code']}],
+                    'formatted_address': 'Google Building 40, 1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA',
+                    'geometry': {
+                        'bounds': {'northeast': {'lat': 37.4226621, 'lng': -122.0829306}, 'southwest': {'lat': 37.4220703, 'lng': -122.0849584}},
+                        'location': {'lat': 37.422388, 'lng': -122.0841883},
+                        'location_type': 'ROOFTOP',
+                        'viewport': {'northeast': {'lat': 37.4237151802915, 'lng': -122.0825955197085}, 'southwest': {'lat': 37.4210172197085, 'lng': -122.0852934802915}}
+                    },
+                    'partial_match': True,
+                    'place_id': 'ChIJj38IfwK6j4ARNcyPDnEGa9g',
+                    'types': ['premise']},
+            {
+                'address_components': [
+                        {'long_name': '1600', 'short_name': '1600', 'types': ['street_number']},
+                        {'long_name': 'Amphitheatre Parkway', 'short_name': 'Amphitheatre Parkway', 'types': ['route']},
+                        {'long_name': 'Mountain View', 'short_name': 'Mountain View', 'types': ['locality', 'political']},
+                        {'long_name': 'Santa Clara County', 'short_name': 'Santa Clara County', 'types': ['administrative_area_level_2', 'political']},
+                        {'long_name': 'California', 'short_name': 'CA', 'types': ['administrative_area_level_1', 'political']},
+                        {'long_name': 'United States', 'short_name': 'US', 'types': ['country', 'political']},
+                        {'long_name': '94043', 'short_name': '94043', 'types': ['postal_code']}],
+                'formatted_address': '1600 Amphitheatre Parkway, Mountain View, CA 94043, USA',
+                'geometry': {
+                    'location': {'lat': 37.4103328, 'lng': -122.0716587},
+                    'location_type': 'ROOFTOP',
+                    'viewport': {'northeast': {'lat': 37.4116817802915, 'lng': -122.0703097197085}, 'southwest': {'lat': 37.4089838197085, 'lng': -122.0730076802915}}},
+                'partial_match': True, 'place_id': 'ChIJj23lMXe3j4ARdBReUp4WTII', 'plus_code': {'compound_code': 'CW6H+48 Mountain View, CA, USA', 'global_code': '849VCW6H+48'},
+                'types': ['street_address']}],
+            'status': 'OK'},
+        "ipinfo": {
+            'ip': '47.185.202.16',
+            'city': 'Plano',
+            'region': 'Texas',
+            'country': 'US',
+            'loc': '33.0198,-96.6989',
+            'org': 'AS5650 Frontier Communications of America, Inc.',
+            'postal': '75026',
+            'timezone': 'America/Chicago'}
+    }
+    return apis[api_name]
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+
+
--- GCP/HW6/web/static/search.js
+++ GCP/HW6/web/static/search.js
@@ -1,44 +1,102 @@
 function onLoad() {
     jQuery(".default-hide").hide()
+    
+    /*TODO remove debug code*/
+    jQuery("#street").val("1600 Ampitheatre Parkway")
+    jQuery("#city").val("Mountain View")
+    jQuery("#state").val("CA")
+}
+
+function onCheckAutoDetect() {
+    if (jQuery("#auto-detect").prop('checked') ) {
+        //TODO do auto-detect
+        clearInputFields()
+        setInputFieldsNoEdit()
+    }
+}
+
+function setInputFieldsNoEdit() {
+    //TODO
+}
+
+function clearInputFields() {
+    jQuery("#street").val("")
+    jQuery("#city").val("")
+    jQuery("#state").val("")
+    jQuery("#auto-detect").prop('checked', false);
 }
 
 function clearForm(){
     jQuery(".default-hide").hide()
-    alert("cleared (TODO - actually empty fields)");
+    clearInputFields()
 }
 function submitForm(){
-    jQuery("#current-weather").show()
-    call_tomorrow_test()
-    call_geocode_test()
+    if (jQuery("#auto-detect").prop('checked') ) {
+        //TODO do auto-detect
+        get_api_info("ipinfo", "json", {}, function(rspObj){
+            //use info and get lat/lng and submit
+            var json = JSON.parse(rspObj.responseText)
+            var loc = json["loc"]
+            var lng = loc.split(",")[1]
+            var lat = loc.split(",")[0]
+            call_tomorrow_weather(lng, lat)
+        });
+    }
+    else {
+        //TODO geocode
+        var addr = jQuery("#street").val()
+        var city = jQuery("#city").val()
+        var state = jQuery("#state").val()
+        var req_data = {
+            /*NOTE: This is the method to call and is treated special*/
+            "address": addr + ", " + city + ", " + state,
+        };
+        get_api_info("geocode", "json", req_data, function(rspObj){
+            var json = JSON.parse(rspObj.responseText)
+            //TODO remove debug (get these from geocode -> results -> 0 -> geometry -> location -> lat/long
+            var lng = 40.75872069597532;
+            var lat = -73.98529171943665;
+            call_tomorrow_weather(lng, lat)
+        });
+    }    
 }
 
-function call_tomorrow_test() {
+function call_tomorrow_weather(lng, lat) {
     var req_data = {
         /*NOTE: This is the method to call and is treated special*/
-        "location": "-73.98529171943665,40.75872069597532",
-        "fields": "temperature",
+        "location": lng + "," + lat,
+        "fields": ["temperature", "pressure", "windSpeed", "visibility", "cloudCover", "uvIndex"],
         "timesteps": "1h",
         "units": "metric"
     };
-    get_api_info("tomorrow", "timelines", req_data);
-}
-function call_geocode_test() {
-    var req_data = {
-        /*NOTE: This is the method to call and is treated special*/
-        "address": "1600 Ampitheatre Parkway, Mountain View, CA",
-    };
-    get_api_info("geocode", "json", req_data);
+    get_api_info("tomorrow", "timelines", req_data, function(rspObj){
+        //alert(rspObj.responseText)
+        jQuery("#humitity-value").text(123)
+        jQuery("#pressure-value").text(321)
+        jQuery("#wind-speed-value").text(321)
+        jQuery("#visibility-value").text(321)
+        jQuery("#cloud-cover-value").text(321)
+        jQuery("#uv-level-value").text(333)
+        
+        jQuery("#current-weather").show()
+    });
 }
 
+function todo_callback(rspObj) {
+    console.log("rspObj length: " + rspObj.responseText.length)
+}
 
 // var url = "/apis/tomorrow/test";
-function get_api_info(api, method, req_data){
+/*
+* callback(this)
+* var data = JSON.parse(this.responseText);
+*/
+function get_api_info(api, method, req_data, callback){
     url = "/apis/" + api
     var req = new XMLHttpRequest();
     req.onreadystatechange = function (){
         if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText);
-            console.log(this.responseText)
+            callback(this)
         }
     }
     req_type = "POST";
