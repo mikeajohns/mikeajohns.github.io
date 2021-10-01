25014d4e894f544659aa58f496e8762383baaa5c
mikeajohns <41806211+mikeajohns@users.noreply.github.com>
2021-10-01 13:57:14 -0500

    Cleanup for submittal

--- GCP/HW6/HTMLServer.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-# subs: dictionary of (key: regex, value: substitution string) pairs
-def serve_subed_file(filepath, subs):
-    with open(filepath) as f:
-        file_str = f.read()
-        for regex in subs:
-            re.sub(regex, subs[regex])
